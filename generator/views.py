21ecf15b41723981498b74b173ca1469c0e97531
James Siegener <160677043+james-siegener@users.noreply.github.com>
2025-04-22 15:48:31 -0400

    tweak tweak tweak

--- generator/views.py
+++ generator/views.py
@@ -14,7 +14,7 @@ def index(request):
 
         if prompt and prompt not in previous_prompts:
 
-            output = model.generate_playlist(prompt)
+            output = model.generate_playlist(prompt, max_length=500)
             tracks = song_id_lookup(output)
             previous_prompts[prompt] = tracks
             if len(previous_prompts) > 15:
@@ -36,15 +36,18 @@ def reset_history(request):
     return redirect('index')
 
 def song_id_lookup(output):
+    print(output)
     songs = output.split('\n')
     songs_clean = [song.split(' -')[0] for song in songs]
     songs_clean = [song.split(' : ')[0] for song in songs_clean]
     songs_clean = list(set(songs_clean))
-
     tracks = []
     for song in songs_clean:
-        matches = song_data[song_data['Song_Name'].str.contains(song, case=False, na=False, regex=False)]
+        matches = song_data[song_data['Song_Name'] == song]
+        if matches.empty:
+            matches = song_data[song_data['Song_Name'].str.contains(song, case=False, na=False, regex=False)]
         try:
+            matches = matches[~matches['Song_Name'].str.contains('lofi', case=False, na=False, regex=False)]
             match = matches.iloc[0]
             tracks.append({
                 'title': match['Song_Name'],
